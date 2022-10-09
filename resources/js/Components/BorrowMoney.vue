5ac98f96f5f616ba5bc65891d99c8edf70434877
Giorgi Trapaidze <giorgitrapaidze@redberry.ge>
2022-10-09 06:53:22 +0400

    wip:demo project *lender making offers *receiver changing offer status *lender dashboard *lender can see his own offers *lender can see filtered loans which can be relative for him

--- resources/js/Components/BorrowMoney.vue
+++ resources/js/Components/BorrowMoney.vue
@@ -1,19 +1,23 @@
 <template>
-    <div class="border border-2 border-gray-300 rounded px-20 py-24">
-        <div class="flex flex-col md:flex-row gap-x-16">
+    <div class="pb-16">
+        <h1 class="text-center text-[38px] text-purple-700 font-bold">აიღეთ სესხი მარტივად!</h1>
+        <div class="flex flex-col md:flex-row gap-32">
             <div class="w-full md:w-1/2 flex flex-col items-center">
                 <div class="flex flex-col w-full mt-4">
                     <div class="mt-4 w-full flex flex-col items-center gap-y-10">
                         <div class="w-full flex flex-col justify-center items-center">
-                            <input class="w-[70px] h-[40px] border-none focus:ring-0" v-model="moneyRange"
-                                   @change="checkForValidInput" type="number" min="0" max="500">
+                            <div>
+                                <input class="w-[70px] h-[40px] border-none focus:ring-0" v-model="moneyRange"
+                                       @change="checkForValidInput" type="text" min="0"
+                                       max="500">{{ currency === 'GEL' ? 'GEL' : 'USD' }}
+                            </div>
                             <input class="w-full" v-model="moneyRange" @change="checkForValidInput" type="range" min="0"
                                    max="500">
                         </div>
-                        <select class="w-full rounded focus:ring-0 mt-3">
-                            <option selected disabled>აირჩიე ვალუტა</option>
-                            <option value="1">USD</option>
-                            <option value="2">GEL</option>
+                        <select v-model="currency" class="w-full rounded focus:ring-0 mt-10">
+                            <option value="აირჩიე ვალუტა" selected disabled>აირჩიე ვალუტა</option>
+                            <option value="USD">USD</option>
+                            <option value="GEL">GEL</option>
                         </select>
                     </div>
                 </div>
@@ -21,12 +25,12 @@
             <div class="w-full md:w-1/2 flex items-center flex-row">
                 <div class="flex mt-4 w-full justify-center items-center flex-col">
                     <div class="mt-4 w-full">
-                        <InputLabel for="card_name" value="პროცენტი"/>
-                        <TextInput id="card_name" type="text" class="mt-1 block w-full border border-black"
-                                   v-model="percent" required placeholder="Soul Goodman"
-                                   autocomplete="card_name"/>
+                        <div class="w-full">
+                            <InputLabel for="return_date" value="დაბრუნების თარიღი"/>
+                            <input class="w-full rounded" id="return_date" type="date">
+                        </div>
                     </div>
-                    <div class="mt-4 w-full">
+                    <div class="mt-12 w-full">
                         <InputLabel for="card_name" value="პროცენტი"/>
                         <TextInput id="card_name" type="text" class="mt-1 block w-full border border-black"
                                    v-model="percent" required placeholder="Soul Goodman"
@@ -35,11 +39,11 @@
                 </div>
             </div>
         </div>
-        <div class="w-full mt-12 flex justify-center items-center flex-col">
-            <div class="w-full max-w-[500px] text-center">
-                <InputLabel for="return_date" value="დაბრუნების თარიღი"/>
-                <input class="w-full" id="return_date" type="date">
-            </div>
+        <div class="mt-20 h-[50px] w-full relative h-[40px">
+            <button
+                class="bg-purple-700 text-[25px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between items-center px-10 py-4 mt-[10px] rounded-xl font-bold hover: text-white cursor-pointer">
+                მოითხოვე სესხი
+            </button>
         </div>
     </div>
 </template>
@@ -57,16 +61,26 @@ export default {
         return {
             moneyRange: 0,
             percent: 10,
+            currency: "აირჩიე ვალუტა",
         }
     },
     methods: {
         checkForValidInput() {
+            this.moneyRange = parseInt(this.moneyRange);
             if (this.moneyRange > 500) {
                 this.moneyRange = 500;
             }
             if (this.moneyRange < 0) {
                 this.moneyRange = 0;
             }
+            if (typeof this.moneyRange !== "number" || isNaN(this.moneyRange)) {
+                this.moneyRange = 0;
+            }
+        }
+    },
+    watch: {
+        'moneyRange': function () {
+            this.checkForValidInput();
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use    \Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,80 +13,100 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::post('/loans/create', function (Request $request) {
-        $validated = $request->validate([
-            'amount' => 'required',
-            'currency ' => 'required',
-            'payment_due_date' => 'required',
-            'demanded_interest_rate' => 'required',
-        ]);
-        $validated['id'] = 2;
-        return $validated;
-    });
-
-    Route::post('/loans/offer/create', function (Request $request) {
-        return $validated = $request->validate([
-            'loan_id' => 'required',
-            'offered_amount' => 'required',
-            'currency ' => 'required',
-            'payment_due_date' => 'required',
-            'demanded_interest_rate' => 'required',
-        ]);
-    });
-
-    Route::get('/loans/offer/accept/:id', function (Request $request) {
-        return ;
-    });
-    Route::get('/loans/offer/reject/:id', function (Request $request) {
-        return $request->all();
-    });
-
-    Route::get('/pending/loans', function (Request $request) {
-        return [
-            [
-                'id' => 1,
-                'status' => 0,
-                'demanded_interest_rate' => 23.42,
-                'amount' => 500,
-                'lender' => null,
-                'payment_due_date' => '12/23/2022',]
-        ];
-    });
-
-    Route::get('/myoffers', function (Request $request) {
-        return [
-            'id' => 1,
-            'status' => 0,
-            'demanded_interest_rate' => 23.42,
-            'amount' => 500,
-            'demand_loan' => [
-                'demanded_interest_rate' => 23.42,
-                'amount' => 500,
-                'payment_due_date' => '12/23/2022',
-                'user' => 'davit'
-            ],
-            'lender' => null,
-            'payment_due_date' => '12/23/2022',
-        ];
-    });
-
-
-    Route::post('/card/store', function ( Request $request) {
-         $validated = $request->validate([
-            'card_name' => 'required',
-            'currency ' => 'required',
-        ]);
-
-         $validated['amount'] = 0;
-
-
-    });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
+Route::middleware('auth:sanctum')->post('/loans/create', function (Request $request) {
+    $ConfigData = json_decode(file_get_contents('../config/fakeData.php'),TRUE);;
+
+    $validated = $request->validate([
+        'amount' => 'required',
+        'currency ' => 'required',
+        'payment_due_date' => 'required',
+        'demanded_interest_rate' => 'required',
+    ]);
+    $validated['id'] = 2;
+    return $validated;
+});
+Route::post('/loans/offer/create', function (Request $request) {
+    $ConfigData = json_decode(file_get_contents('../config/fakeData.php'),TRUE);
+    $name = 'test';
+    $reputation = 233;
+    $validated = $request->validate([
+        'loan_id' => 'required',
+        'offered_interest_rate' => 'required',
+        'offered_amount' => 'required',
+        'currency ' => 'required',
+        'payment_due_date' => 'required',
+    ]);
+    $validated['lender']['name'] = $name;
+    $validated['lender']['reputation'] = $reputation;
+    $validated['id'] = rand(1231,78733);
+    foreach ($ConfigData['loans'] as $key => $value) {
+        if($value['id'] == $validated['loan_id']) {
+            $ConfigData['loans'][$key]['offers'][] = $validated;
+            $ConfigData['myoffers'][] = $validated;
+            file_put_contents('../config/fakeData.php', json_encode($ConfigData));
+        }
+    }
+    return ['status' => true, 'data' => $validated];
 
 
+
+});
+Route::get('/loans/offer/accept/{id}/{offer_id}', function ($id,$offer_id) {
+    $ConfigData = json_decode(file_get_contents('../config/fakeData.php'),TRUE);;
+    foreach ($ConfigData['loans'] as $key => $value) {
+        if(!empty($value['offers'])) {
+            foreach ($value['offers'] as $key2 => $value2) {
+                if($value2['id'] == $offer_id) {
+                    $value2['status'] = 1;
+                    $ConfigData['loans'][$key]['offers'][$key2]['status'] = 1;
+                    $value['status'] = 1;
+                    $value['detail'] = [
+                        'total_amount' => $value2['offered_amount'] * $value2['offered_interest_rate']/100 + $value2['offered_amount'],
+                        'interest_rate' => 15,
+                        'left_base_amount_to_pay' => $value2['offered_amount'],
+                        'left_interest_amount_to_pay' => $value2['offered_amount'] * $value2['offered_interest_rate']/100,
+                        'payment_due_date' => '12/23/2022',
+                    ];
+                    $ConfigData['loans'][$key]['status'] = 1;
+                    file_put_contents('../config/fakeData.php', json_encode($ConfigData));
+                    return ['status' => true, 'loan' => $value];
+                }
+            }
+        }
+    };
+    return ['status' => false];
+});
+Route::get('/loans/offer/reject/{id}/{offer_id}', function ($id,$offer_id) {
+    $ConfigData = json_decode(file_get_contents('../config/fakeData.php'),TRUE);
+    foreach ($ConfigData['loans'] as $key => $value) {
+        if(!empty($value['offers'])) {
+            foreach ($value['offers'] as $key2 => $value2) {
+                if($value2['id'] == $offer_id) {
+                    $value2['status'] = 2;
+                    $ConfigData['loans'][$key]['offers'][$key2]['status'] = 2;
+                    file_put_contents('../config/fakeData.php', json_encode($ConfigData));
+                    return ['status' => true, 'loan' => $ConfigData['loans'][$key]['offers'][$key2]];
+                }
+            }
+        }
+    };
+    return ['status' => false];
+});
+Route::get('/pending/loans', function (Request $request) {
+    $ConfigData = json_decode(file_get_contents('../config/fakeData.php'),TRUE);
+    $max_limit = 400;
+    $filtered_arr = [];
+    foreach ($ConfigData['loans'] as $key => $value) {
+        if($value['status'] == 0 && $value['amount'] <= $max_limit) {
+            $filtered_arr[] = $value;
+        };
+    }
+    return $filtered_arr;
+});
+Route::middleware('auth:sanctum')->get('/myoffers', function (Request $request) {
+    $ConfigData = json_decode(file_get_contents('../config/fakeData.php'),TRUE);
+    return $ConfigData['myOffers'];
+});
